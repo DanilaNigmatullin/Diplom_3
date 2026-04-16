de171ef96a34da465589258bc18143449288e362
Danila Nigmatullin <mr.filchenkov94@mail.ru>
2026-04-16 14:22:46 +0300

    Fix: serialize user via object, add @Step, add allure-results

--- src/test/java/api/UserApiClient.java
+++ src/test/java/api/UserApiClient.java
@@ -1,6 +1,8 @@
 package api;
 
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
+import model.UserModel;
 
 import static io.restassured.RestAssured.given;
 
@@ -8,14 +10,17 @@ public class UserApiClient {
 
     private static final String BASE_URL = "https://stellarburgers.education-services.ru";
 
+    @Step("Создать тестового пользователя через API")
     public String createUser(String email, String password, String name) {
+        UserModel user = new UserModel(email, password, name);
         Response response = given()
                 .header("Content-type", "application/json")
-                .body("{\"email\":\"" + email + "\",\"password\":\"" + password + "\",\"name\":\"" + name + "\"}")
+                .body(user)
                 .post(BASE_URL + "/api/auth/register");
         return response.jsonPath().getString("accessToken");
     }
 
+    @Step("Удалить тестового пользователя через API")
     public void deleteUser(String accessToken) {
         given()
                 .header("Authorization", accessToken)
